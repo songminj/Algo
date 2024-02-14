bfd0efea61bdb87623ddcb7758e7f5a8717733e1
MJ <104004869+songminj@users.noreply.github.com>
2024-02-14 20:12:07 +0900

    [Bronze I] Title: 색종이, Time: 1732 ms, Memory: 38436 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -7,14 +7,14 @@ arr = list([0]*(1001) for _ in range(1001))
 for i in range(1, num+1):
     x1, y1, w, h = map(int, sys.stdin.readline().strip().split())
     # area[i] = loc + [abs((loc[2]+loc[0]) * loc[3]-loc[1])]
-    for p in range(w):
-        for q in range(h):
-            arr[x1+p][y1+q] = i
-
+    # for p in range(w):
+    #     for q in range(h):
+    #         arr[x1+p][y1+q] = i
+    for p in range(h):
+        arr[y1+p][x1:x1+w] = [i] * w
 
 for target in range(1, num+1):
     cnt = 0
     for i in range(1001):
         cnt += arr[i].count(target)
     print(cnt)
-

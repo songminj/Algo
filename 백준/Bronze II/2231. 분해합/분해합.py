2972dd06476a557fdbbbe03c351de8c0effe6623
MJ <104004869+songminj@users.noreply.github.com>
2024-02-15 00:10:29 +0900

    [Bronze II] Title: 분해합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze II/2231. 분해합/분해합.py
+++ 백준/Bronze II/2231. 분해합/분해합.py
@@ -5,17 +5,16 @@ num_len = len(str(num))
 min_sum = num
 
 if int(num) >= (num_len*9):
-    for i in range(num-(num_len*9)-1, num):
-        each = list(map(int, str(i)))
-        if (i + sum(each)) == num:
-            min_sum = i
-            break
+    rg = range(num-(num_len*9)-1, num)
 else:
-    for i in range(1, num):
-        each = list(map(int, str(i)))
-        if (i + sum(each)) == num:
-            min_sum = i
-            break
+    rg = range(1, num)
+
+for i in rg:
+    each = list(map(int, str(i)))
+    if (i + sum(each)) == num:
+        min_sum = i
+        break
+
 if min_sum == num:
     print(0)
 else:

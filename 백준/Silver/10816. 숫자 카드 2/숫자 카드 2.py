4e7c19646b0dc7915b03fe400328a723dd9679e6
MJ <104004869+songminj@users.noreply.github.com>
2024-02-17 21:33:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1032 ms, Memory: 115624 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,17 @@
+import sys
+
+# n : card
+n = int(sys.stdin.readline().strip())
+cards = list(map(int, sys.stdin.readline().strip().split()))
+
+# target num
+tnum = int(sys.stdin.readline().strip())
+targets = list(map(int, sys.stdin.readline().strip().split()))
+targets_count = {i: 0 for i in targets}
+
+# counting
+for i in range(n):
+    if cards[i] in targets_count.keys():
+        targets_count[cards[i]] += 1
+for i in targets:
+    print(targets_count[i], end=' ')

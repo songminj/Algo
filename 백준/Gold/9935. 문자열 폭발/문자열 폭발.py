ccd031610f9722e5a055acbf2b6c12ac8b9b4805
MJ <104004869+songminj@users.noreply.github.com>
2024-02-29 17:20:46 +0900

    [Gold IV] Title: 문자열 폭발, Time: 896 ms, Memory: 49136 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,19 @@
+import sys
+
+words = list(sys.stdin.readline().strip())
+bomb = sys.stdin.readline().strip()
+len_words = len(words)
+len_bomb = len(bomb)
+stack = []
+for i in range(len(words)):
+    stack.append(str(words[i]))
+
+    if len(stack) >= len_bomb:
+        if ''.join(stack[-len_bomb:]) == bomb:
+            for i in range(len_bomb):
+                stack.pop()
+else:
+    if len(stack) == 0:
+        stack = 'FRULA'
+
+print(''.join(stack))

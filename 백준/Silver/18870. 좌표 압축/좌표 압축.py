53fafbe4036f4df1096f4810798dd4f0a3a3ecd8
MJ <104004869+songminj@users.noreply.github.com>
2024-03-01 13:59:27 +0900

    [Silver II] Title: 좌표 압축, Time: 2016 ms, Memory: 155276 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,24 +1,27 @@
 import sys
 
-
-def binary_search(target):
-    start = 0
-    end = len(s_nums) - 1
-    while start <= end:
-        mid = (start + end) // 2
-
-        if s_nums[mid] == target:
-            return mid
-        elif s_nums[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return
+# 
+# def binary_search(target):
+#     start = 0
+#     end = len(s_nums) - 1
+#     while start <= end:
+#         mid = (start + end) // 2
+# 
+#         if s_nums[mid] == target:
+#             return mid
+#         elif s_nums[mid] > target:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
+#     return
 
 
 n = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().strip().split()))
 s_nums = sorted(list(set(nums)))
-# print(s_nums)
+dic = {s_nums[i]: i for i in range(len(s_nums))}
+
 for i in nums:
-    print(binary_search(i), end = ' ')
+    print(dic[i], end=' ')
+
+# print(sort_nums)

ff8caaee8c455585977d3ae031d9c1d77a5b0a6d
MJ <104004869+songminj@users.noreply.github.com>
2024-03-01 13:56:21 +0900

    [Silver II] Title: 좌표 압축, Time: 4656 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def binary_search(target):
+    start = 0
+    end = len(s_nums) - 1
+    while start <= end:
+        mid = (start + end) // 2
+
+        if s_nums[mid] == target:
+            return mid
+        elif s_nums[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return
+
+
+n = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().strip().split()))
+s_nums = sorted(list(set(nums)))
+# print(s_nums)
+for i in nums:
+    print(binary_search(i), end = ' ')

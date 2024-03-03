32ffb7a86ee06aded3b4eb8503f51a75bbe7fdb1
MJ <104004869+songminj@users.noreply.github.com>
2024-03-03 17:26:42 +0900

    [Silver III] Title: N과 M (5), Time: 140 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -5,12 +5,14 @@ n, m = map(int, input().split())
 digits = list(map(int, input().split()))
 digits.sort()
 
-def perm(m, lst):
+def perm(m, lst, visited):
     if len(lst) == m:
         print(' '.join(lst))
     for i in range(n):
-        if str(digits[i]) not in lst:
-            perm(m, lst + [str(digits[i])])
+        if visited[i] == 0:
+            visited[i] = 1
+            perm(m, lst + [str(digits[i])], visited)
+            visited[i] = 0
 
-
-perm(m, [])
+visited = [0] * n
+perm(m, [], visited)

7bb481e552226af129384b9e8dd02a01b1a968ae
MJ <104004869+songminj@users.noreply.github.com>
2024-03-03 18:46:19 +0900

    [Silver II] Title: N과 M (12), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,17 @@
+# n과 m (12)
+
+n, m = map(int, input().split())
+digits = sorted(list(map(int, input().split())))
+def perm(idx, m, lst):
+    if len(lst) == m:
+        print(' '.join(lst))
+        return
+    before = 0
+    for i in range(idx, n):
+        if before != digits[i]:
+            before = digits[i]
+            perm(i, m, lst + [str(digits[i])])
+
+
+visited = [False] * n
+perm(0, m, [])

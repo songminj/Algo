bc1a5d092f8cb3ff10c32819aa6b0c11e4437b7e
MJ <104004869+songminj@users.noreply.github.com>
2024-03-12 22:16:17 +0900

    [Silver I] Title: 그림, Time: 340 ms, Memory: 35528 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,17 +1,23 @@
 import sys
-sys.setrecursionlimit(10**8)
+from collections import deque
 di = [0, 1, 0, -1]
 dj = [1, 0, -1, 0]
 
-def dfs(i, j):
-    global size
+def bfs(i, j):
+    size = 1
+    queue = deque([(i, j)])
     visited[i][j] = True
-    size += 1
-    for k in range(4):
-        ni = i + di[k]
-        nj = j + dj[k]
-        if 0 <= ni < n and 0<= nj < m and arr[ni][nj] == '1' and not visited[ni][nj]:
-            dfs(ni, nj)
+    while queue:
+        newi, newj = queue.popleft()
+        for k in range(4):
+            ni = newi + di[k]
+            nj = newj + dj[k]
+            if 0<= ni < n and 0 <= nj < m:
+                if arr[ni][nj] == '1' and not visited[ni][nj]:
+                    visited[ni][nj] = True
+                    size += 1
+                    queue.append((ni, nj))
+    return size
 
 n, m = map(int, sys.stdin.readline().strip().split())
 arr = [list(sys.stdin.readline().strip().split()) for _ in range(n)]
@@ -23,9 +29,7 @@ for i in range(n):
     for j in range(m):
         size = 0
         if arr[i][j] == '1' and not visited[i][j]:
-            dfs(i, j)
-            if max_size < size:
-                max_size = size
+            max_size = max(bfs(i, j), max_size)
             cnt += 1
 print(cnt)
 print(max_size)

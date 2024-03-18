12c2d585040be0caa00bb485ac061c05e6fc5c05
MJ <104004869+songminj@users.noreply.github.com>
2024-03-18 22:26:40 +0900

    [Gold V] Title: LCS, Time: 364 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -3,12 +3,16 @@ import sys
 a = sys.stdin.readline().strip()
 b = sys.stdin.readline().strip()
 al, bl = len(a), len(b)
-dp = [[0] * (bl+1) for _ in range(al+1)]
+dp = [0] * (al)
 
-for i in range(1, al+1):
-    for j in range(1, bl+1):
-        if a[i-1] == b[j-1]:
-            dp[i][j] = dp[i-1][j-1] +1
+for w in b:
+    cnt = 0
+    for idx in range(al):
+        if w == a[idx]:
+            if cnt < dp[idx]:
+                cnt = dp[idx]
+            else:
+                dp[idx] = cnt +1
         else:
-            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-print(dp[-1][-1])
+            cnt = max(cnt, dp[idx])
+print(max(dp))

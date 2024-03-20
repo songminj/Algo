e80f462303d174ef8f8dec2dccff1dc89524bba0
MJ <104004869+songminj@users.noreply.github.com>
2024-03-20 09:27:03 +0900

    [Gold V] Title: 강의실 배정, Time: 356 ms, Memory: 62724 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -6,16 +6,18 @@ n = int(sys.stdin.readline())
 time = []
 visited = [False] * n
 for _ in range(n):
-    t = list(map(int, sys.stdin.readline().strip().split()))
+    t = tuple(map(int, sys.stdin.readline().strip().split()))
     time.append(t)
 time.sort()
 
-# time을 정렬한 후 최소로 강의실을 이용하는 case를 res++ 해줌
+# 시작시간 < 이용중인 회의실의 끝나는시간 이면 새로운 회의실을 열어주고 
 room = []
 heappush(room, time[0][1])
 for i in range(1, n):
     if time[i][0] < room[0]:
         heappush(room, time[i][1])
+    # 시작시간 > 이용중인 회의실이 끝나는시간 (= 지금 비어있는 회의실이 있으면) 이면
+    # 이용했던 회의실 시간을 빼주고 새로운 회의의 끝나는 시간을 넣어준다. 
     else:
         heappop(room)
         heappush(room, time[i][1])

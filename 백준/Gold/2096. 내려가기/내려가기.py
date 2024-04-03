a53283b88326609bd1c0ff7337e69f04be4c514b
MJ <104004869+songminj@users.noreply.github.com>
2024-04-03 20:27:11 +0900

    [Gold V] Title: 내려가기, Time: 296 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -3,28 +3,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [list(map(int, input().split())) for _ in range(n)]
-max_dp = [dp[0][:]] + [[0]*3]
-min_dp = [dp[0][:]] + [[0]*3]
+max_dp = [0] * 3
+min_dp = [0] * 3
 
-for i in range(1, n):
-    if i % 2 ==1 :
-        max_dp[1][0] = max(max_dp[0][0], max_dp[0][1]) + dp[i][0]
-        max_dp[1][1] = max(max_dp[0][0], max_dp[0][1], max_dp[0][2]) + dp[i][1]
-        max_dp[1][2] = max(max_dp[0][1], max_dp[0][2]) + dp[i][2]
-        min_dp[1][0] = min(min_dp[0][0], min_dp[0][1]) + dp[i][0]
-        min_dp[1][1] = min(min_dp[0][0], min_dp[0][1], min_dp[0][2]) + dp[i][1]
-        min_dp[1][2] = min(min_dp[0][1], min_dp[0][2]) + dp[i][2]
-    else :
-        max_dp[0][0] = max(max_dp[1][0], max_dp[1][1]) + dp[i][0]
-        max_dp[0][1] = max(max_dp[1][0], max_dp[1][1], max_dp[1][2]) + dp[i][1]
-        max_dp[0][2] = max(max_dp[1][1], max_dp[1][2]) + dp[i][2]
-        min_dp[0][0] = min(min_dp[1][0], min_dp[1][1]) + dp[i][0]
-        min_dp[0][1] = min(min_dp[1][0], min_dp[1][1], min_dp[1][2]) + dp[i][1]
-        min_dp[0][2] = min(min_dp[1][1], min_dp[1][2]) + dp[i][2]
-
-
-if n % 2 == 1:
-    print(max(max_dp[0]), min(min_dp[0]))
-else:
-    print(max(max_dp[1]), min(min_dp[1]))
+for i in range(n):
+    a, b, c = map(int, input().split())
+    max_dp = [max(max_dp[0], max_dp[1]) + a,
+              max(max_dp) + b,
+              max(max_dp[1], max_dp[2]) + c]
+    min_dp = [min(min_dp[0], min_dp[1]) + a,
+              min(min_dp) + b,
+              min(min_dp[1], min_dp[2]) + c]
+print(max(max_dp), min(min_dp))

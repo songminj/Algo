60a8367c2d316840eb1bf89d1a5c6003809f1217
MJ <104004869+songminj@users.noreply.github.com>
2024-04-10 23:34:07 +0900

    [Gold IV] Title: 오큰수, Time: 932 ms, Memory: 163164 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -3,10 +3,12 @@ input = sys.stdin.readline
 
 N = int(input())
 nums = list(map(int, input().split()))
-NGE = [-1] * N
+NGE = ['-1'] * N
+# index를 stack에 넣는다
 stack = [0]
 for i in range(1, N):
+    # 왼쪽에서 큰 수가 나올 때 까지 while문을 돌린다
     while stack and nums[stack[-1]] < nums[i]:
-        NGE[stack.pop()] = nums[i]
+        NGE[stack.pop()] = str(nums[i])
     stack.append(i)
-print(*NGE)
+print(' '.join(NGE))

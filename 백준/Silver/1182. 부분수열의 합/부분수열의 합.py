8552fae756a19f7fdcb66eb02e3a4ed009221a3a
MJ <104004869+songminj@users.noreply.github.com>
2024-06-14 17:27:07 +0900

    [Silver II] Title: 부분수열의 합, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,17 +1,19 @@
 # 1182번 부분수열의 합
 
-import sys
-input = sys.stdin.readline
-
 n, s = map(int, input().strip().split())
-arr = list(map(int, input().strip().split()))
+arr = sorted(list(map(int, input().strip().split())))
+res = 0
+def dfs(cnt, k):
+    global res
+    if k == n:
+        if cnt == s:
+            res += 1
+        return
+    dfs(cnt+arr[k], k+1)
+    dfs(cnt, k+1)
 
-cnt = 0
-for m in range(1, 1 << n):
-    subset_sum = 0
-    for i in range(n):
-        if m & (1 << i):
-            subset_sum += arr[i]
-    if subset_sum == s:
-        cnt += 1
-print(cnt)
+dfs(0, 0)
+if s == 0:
+    print(res-1)
+else:
+    print(res)

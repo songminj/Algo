8903bdb2ddc64639abc28d271a28b7028c3fbda5
MJ <104004869+songminj@users.noreply.github.com>
2024-06-14 16:18:45 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,15 @@
+# 5622번 다이얼
+
+dial = {'A':2, 'B':2, 'C':2,
+        'D':3, 'E':3, 'F':3,
+        'G':4, 'H':4, 'I':4,
+        'J':5, 'K':5, 'L':5,
+        'M':6, 'N':6, 'O':6,
+        'P':7, 'Q':7, 'R':7, 'S':7,
+        'T':8, 'U':8, 'V':8,
+        'W':9, 'X':9, 'Y':9, 'Z':9}
+alpha = input().strip()
+res = len(alpha)
+for w in alpha:
+    res += dial[w]
+print(res)

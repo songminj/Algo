e0ea8f060ccf543e272df3ff99ac9b3b7ea20407
MJ <104004869+songminj@users.noreply.github.com>
2024-06-23 15:29:39 +0900

    [Silver I] Title: 스타트와 링크, Time: 1404 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 def perm(lst):
     cnt = 0
     for i in range(n-1):
-        for j in range(i, n):
+        for j in range(i+1, n):
             x, y = lst[i], lst[j]
             cnt += arr[x][y] + arr[y][x]
     return cnt
@@ -13,9 +13,12 @@ res = 1e9
 N= int(input())
 arr = [list(map(int, input().strip().split())) for _ in range(N)]
 n = N//2
-nums = [i for i in range(N)]
-for teams in combinations(nums, n):
-    others = [ i for i in range(N) if i not in teams]
+nums = [i for i in range(1, N)]
+
+for t in combinations(nums, N-n-1):
+    teams = [0]
+    teams.extend(t)
+    others = [ i for i in range(1, N) if i not in teams]
     a, b = perm(teams), perm(others)
     diff = abs(a-b)
     if diff < res:

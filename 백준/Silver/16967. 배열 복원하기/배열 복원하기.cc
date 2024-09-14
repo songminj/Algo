5bbd9f3f4562cc8fb17e2d1b1e7f10edcde1c9f4
MJ <104004869+songminj@users.noreply.github.com>
2024-09-15 00:52:48 +0900

    [Silver III] Title: 배열 복원하기, Time: 24 ms, Memory: 3476 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.cc
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.cc
@@ -5,6 +5,8 @@ using namespace std;
 
 int H, W, X, Y;
 int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
 	cin >> H >> W>> X>> Y;
 	vector<vector<int>> arr(X+H, vector<int>(Y+W,0));
 	for (int i = 0; i < X+H; i++) {

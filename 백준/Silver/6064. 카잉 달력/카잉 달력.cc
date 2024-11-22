7a5635ffdb7272cfd9ac2d1d29ef7d7c38a732e6
MJ <104004869+songminj@users.noreply.github.com>
2024-11-22 10:21:27 +0900

    [Silver I] Title: 카잉 달력, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.cc
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -2,13 +2,10 @@
 
 using namespace std;
 
-int tc;
-int M, N, x, y;
-
 // 최대공약수
 int gcd(int a, int b) {
-	if (a % b == 0) {
-		return b;
+	if  (b == 0) {
+		return a;
 	}
 	return gcd(b, a % b);
 }
@@ -18,31 +15,29 @@ int lcm(int a, int b) {
 	return (a * b) / gcd(a, b);
 }
 
-void solution() {
-	int result = -1;
-
-	for (int i = x; i <= lcm(M, N); i += M) {
+int solution(int M, int N, int x, int y) {
+	int max = lcm(M, N);
+	for (int i = x; i <= max; i += M) {
 		int ny = i % N;
 		if (ny == 0) {
 			ny = N;
 		}
 		if (ny == y) {
-			result = i;
-			break;
+			return i;
 		}
 	}
 
-	cout << result << "\n";
+	return -1;
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-
+	int M, N, x, y, tc;;
 	cin >> tc;
 	while (tc--) {
 		cin >> M >> N >> x >> y;
-		solution();
+		cout << solution(M, N, x, y) << "\n";
 	}
 	return 0;
 }

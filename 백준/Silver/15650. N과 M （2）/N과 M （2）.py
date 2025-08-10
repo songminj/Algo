b52c02d70f29df65ee45fff096dbf5435c4cf14c
MinJung Song <104004869+songminj@users.noreply.github.com>
2025-08-10 11:14:44 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,20 +1,15 @@
-def dfs(result, i):
-    # 조합의 길이가 M이 되면 결과에 추가
-    if len(result) == M:
-        print(*result)
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().strip().split())
+
+def nm(val, cnt, idx):
+    if cnt == M:
+        print(*val)
         return
+    for i in range(idx, N+1):
+        if i not in val:
+            nxt = val + [i]
+            nm(nxt, cnt +1, i)
 
-    # 가능한 모든 선택지에 대해 탐색
-    for w in range(i, N + 1):
-        # 이미 선택한 수는 건너뜀
-        if w in result:
-            continue
-        # 현재 수를 선택하고 다음 위치로 이동
-        dfs(result + [w], w+1)
-
-
-# 입력 받기
-N, M = map(int, input().split())
-
-# DFS로 순열 구하기
-dfs([], 1)
+nm([], 0, 1)

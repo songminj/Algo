12086132a2fb308b2556dac64f3ad2e2619610ad
MinJung Song <104004869+songminj@users.noreply.github.com>
2025-10-22 10:19:22 +0900

    [Gold V] Title: 1로 만들기 2, Time: 2104 ms, Memory: 380760 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,18 @@
+# 12852번 1로 만들기 2
+
+N = int(input())
+dp = [[0, []] for _ in range(N + 1)]
+dp[1][0] = 0
+dp[1][1] = [1]
+
+for i in range(2, N + 1):
+    dp[i][0] = dp[i-1][0] + 1
+    dp[i][1] = dp[i-1][1] + [i]
+    if i % 3 == 0 and dp[i // 3][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i // 3][0] + 1
+        dp[i][1] = dp[i // 3][1] + [i]
+    if i % 2 == 0 and dp[i // 2][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i // 2][0] + 1
+        dp[i][1] = dp[i // 2][1] + [i]
+print(dp[N][0])
+print(*reversed(dp[N][1]))
